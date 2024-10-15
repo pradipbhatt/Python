397eb1e44dd4a87edef70ef861daa97dbcc385b3
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 15:09:57 +0545

    I learn the new way to access the array elements

---
+++ Basics/Output.py
@@ -0,0 +1,1 @@
+# Python - Output Variables
--- Basics/variables.py
+++ Basics/variables.py
@@ -17,3 +17,29 @@ print(myVAr)
 
 # Snake case variable are my_name = "Pradip"
 
+# Python variables assingning multiple variables
+
+x,y,z = "Hello", 10 , 11.11
+
+print(x)
+print(y)
+print(z)
+
+# MUltiple variables haru lekhda x,y,z,... = "Hello", 10 , . . . yesto way follow garnu parne cha
+
+
+# we also make the array to hold the variables values then assingn to to the variables.
+fruits = ['Apple',"Banana","MIlk"]
+
+x,y,z=fruits
+
+print(x)
+print(y)
+print(z)
+
+students = [ "Pradip",
+            18,
+            "FWU"
+            ]
+x,y,z=students
+print(y)
