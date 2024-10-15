c48a572c439f9dfb8f1e7b53585dfa131eeaf809
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 16:45:29 +0545

    int float and complex type data studied

---
+++ Basics/Python_numbers.py
@@ -0,0 +1,45 @@
+# There are three numeric types in python:int float complex
+
+x = 1
+y = 11.11
+z = 1 + 2j 
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# int or integer, is a whole number , -ve,+ve,without decimals and of unlimited length.
+
+x=1
+y=383838383838383838383838
+z=-939393939393938383
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Type conversions
+x=1
+y=2.8
+z=1j
+
+# Convert int ---> float
+
+a=float(x)
+print(a)
+
+# Convert float to int 
+
+b=int(y)
+print(b)
+
+# Convert int --->complex
+
+c=complex(x)
+print(c)
+
+print(type(c))
+
+
+
