84adae28d5fab270dbf66801e5dbb1aaebdbffa7
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 15:21:12 +0545

    learn how to concatenate the numers using the str

--- Basics/Output.py
+++ Basics/Output.py
@@ -31,3 +31,26 @@ z = "dear lily"
 print(x+y+z) # but here is hack we cant concatenate srings with the numbers and floats we just concatenate strings with strings 
 # because + operator only works for strings
 
+x=3
+y=5
+print(x+y) # here + operator is used for summation of these numbers
+
+# we can convert the numbers to strings using str() function
+
+x=3
+y=5
+print(str(x)+str(y)) #numbers lae concatenate garna ko lage we use str() function
+
+
+
+# Python - Strings
+# we can also use the * operator to repeat the strings
+x="Python "
+print(x*3) # here * operator is used for repetition of the string output = python python python
+
+
+# best way to output the multiple variables is using the comma operator
+
+x=5
+y = "John shelby" #string takes white speaces as well
+print(x,y)
