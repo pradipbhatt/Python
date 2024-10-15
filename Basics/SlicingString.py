ed9ae57d14b058fbb600aa9fc8d45359ac5c40dd
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 17:27:56 +0545

    we can slide the string from the start to end and end to strt as well

---
+++ Basics/SlicingString.py
@@ -0,0 +1,15 @@
+#Hami haru return garna sakxau characters by slice
+
+#lets practise slicing
+
+a = "Hello lilly"
+
+print(a[0:5]) #THis return the string of the range [0,5]
+
+print(a[5:10]) #THis return the string of the range [5,10]
+
+print(a[0:]) #THis return the string from the start to the end
+
+print(a[:5]) #THis return the string from the start to the 5th index
+
+print(a[-4:]) #THis return the string from the last index to the end
