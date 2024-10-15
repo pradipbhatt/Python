85ef73ea05d27a679b9795cf6eb404afaedd695d
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 16:56:51 +0545

    typecasting learned

---
+++ Basics/PythonTypeCasting.py
@@ -0,0 +1,23 @@
+# There may be times when we want to specify a variable, this may be done with typecasting. BEing python oop launguage and such as it uses classes to define the data types including its primitive types
+
+# typecasting garda we need constructor functions
+
+# Typecasting of the integers
+
+x=int(1)
+y=int(2.3333)
+z=int("34")
+
+print(x,y,z)
+print(type(x),type(y),type(z))  #here the types of each variables have integer types
+
+# Typecasting of the floats
+x=float(33)
+y=float("2.5")
+z=float(True)
+
+print(x,y,z)
+print(type(x),type(y),type(z))  # here the types of each variables have float types
+
+# Same way we may type cast to string by using x = str("hello lilly")
+
---
+++ Basics/RandomNum.py
@@ -0,0 +1,5 @@
+import random
+
+print(random.randrange(1,10))
+
+print(random.uniform(1.0, 5.0))
