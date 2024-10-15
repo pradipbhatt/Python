203c8321adc7e7930366ac8876c4fd7913ecc241
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 14:45:03 +0545

    Variables done

---
+++ main.py
@@ -0,0 +1,63 @@
+# # # # # This is my first python program
+# # # # # print("I am from python");
+# # # # # print("HEllo i am listening");
+
+# # # # # Variables
+# # # # # name = "John Doe";
+# # # # # age = 30;
+# # # # # isEmployed = True;
+
+# # # # # print(name)
+
+# # # # first_name = "bro"
+# # # # print(f"Hello{first_name}")
+
+# # # # first_name = "bro"
+# # # # food = "rice"
+# # # # email = "bhattsammar04@gmail.com"
+
+# # # # print(f"Hello, My name is {first_name}. \n I love eating {food}.\n My email address is {email}.")
+
+# # # # INtegers
+# # # age =18
+# # # if(age>18) :{
+# # #     print("You are eligible to vote")
+# # # }
+# # # else : {
+# # #     print("You are not eligible to vote")
+# # # }
+
+# # # # Floats
+# # # price = 10.39;
+# # # gpa=3.2;
+
+# # # if(price>10):{
+# # #     print("The price is too high")
+# # # }
+# # # print(f"The price is {price}")
+# # # print(f"The gpa is {gpa}")
+
+# # # distance = 5.5
+# # # print(f"YOou need to travel to {distance}")
+
+# # # # Booleans
+
+# # # isStudent = True 
+# # # if isStudent:
+# # #     print("You are a student")
+# # # else:
+# # #     print("You are not a student") 
+
+# # forSale = False
+
+# # if forSale:
+# #     print("The item is for sale")
+# # else:
+# #     print("The item is not for sale")
+
+# is_online = True
+
+# if is_online:
+#     print("You are online")
+# else:
+#     print("You are offline")
