7e764a172458a6b2b56f3c215a793cf4ebe9d0a0
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 17:40:37 +0545

    🚀 RELEASE: the first version

---
+++ Basics/ModifyStrings.py
@@ -0,0 +1,35 @@
+#Python sanga inbuilt methods hunxan for modofy the strings
+
+#Upper case ko herau 
+
+a = "Hey,Are u too from radiant?"
+
+print(a.upper()) #Returns all the characters in the UPPER CASE
+
+#Lower case ko herau
+
+a = "Hey, Are u too from radiant?"
+
+print(a.lower()) #Returns all the characters in the lower CASE
+
+
+
+
+#Now euta yesto function padahu jasle automatically starting ko  whitespaces hetaucha
+
+a = "       Hey, Are u too from Radiant dear imaginaary lady?"
+print(a.strip())
+
+
+
+#Let's study about the Replace function
+
+a = "Hey, Are u too from radiant?"
+
+print(a.replace("radiant","radiant girl")) #This function replaces the first argument with the second argument in the string
+
+
+
+#Now let's study the SPlit FUnction using the split() method
+
+x="Hey dear, why u are so much cute"
