cf1ccf27951536b93ef034505885ad13f42e5260
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 15:16:06 +0545

    I learnt about the concatenation magic where in py conatenate use only in the strings

--- Basics/Output.py
+++ Basics/Output.py
@@ -1,1 +1,33 @@
 # Python - Output Variables
+
+# hamilae tha vayeko print() function also used to output variables
+
+x = "Too good that's why she left"
+print(x)
+
+# now add the many variables and values
+
+x = "Python"
+y= "JS"
+z = 11.11
+
+print(x,y,z) #here we print the many varaibles at once
+
+# we can also use the format() function to print variables
+
+x = "Python"
+y= "JS"
+z = 11.11
+
+print(f"I love {x} and {y}, the average of {z} is {z/2}") #here we print the many varaibles at once using format() function
+# Tya agadi use vayeko f stants for format function
+
+# Lets use the + operator to assign the multiple variables
+
+x = "Python"
+y= "JS"
+z = "dear lily"
+
+print(x+y+z) # but here is hack we cant concatenate srings with the numbers and floats we just concatenate strings with strings 
+# because + operator only works for strings
+
