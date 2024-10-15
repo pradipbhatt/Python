3a2e5b0a6d4ef113e83c84f61251c01437f8109c
pradipbhatt <bhattsammar04@gmail.com>
2024-10-15 15:04:16 +0545

    There are three cases camel pascal and snake in variable names

---
+++ Basics/if_else.py
@@ -0,0 +1,7 @@
+age = 18
+
+# Check if age is greater than or equal to 10
+if (age <= 10):
+  print(f"Hello you're age is {age}")
+else:
+    print(f"Hello, World! your age is {age}")
---
+++ Basics/variables.py
@@ -0,0 +1,19 @@
+myvar = "pradip"
+my_var = "John"
+# _my_var = "Thomas"
+myVAr = "Tommy"
+# MYVAR = "Shelby"
+my_var1 = "Aaron"
+
+print(my_var)
+print(myvar)
+print(myVAr)
+
+
+# Camel case variable are myNAme = "Pradip"
+
+# Pascal case variable are MyNAme = "Pradip"
+
+
+# Snake case variable are my_name = "Pradip"
+
