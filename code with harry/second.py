34369c665bec2ebfef19d06c72b1cbaa801d9aaa
pradipbhatt <bhattsammar04@gmail.com>
2024-12-23 16:30:38 +0545

    feat: :art: i have added the comment for testing the git commit using the conventional commit

--- code with harry/second.py
+++ code with harry/second.py
@@ -3,4 +3,6 @@ import pyjokes # type: ignore
 joke = pyjokes.get_joke()
 print(joke)
 
+# Output: "Why did the chicken cross the road? To get to the other side."
+
 # here we need to install the module pyjokes globally using pip install pyjokes --break-system-packages
