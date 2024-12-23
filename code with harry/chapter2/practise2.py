3e40adb7683674308c19dee3fa7e592db2ae9cf9
pradipbhatt <bhattsammar04@gmail.com>
2024-12-23 22:14:21 +0545

    practise set for the data types operators done

---
+++ code with harry/chapter2/practise2.py
@@ -0,0 +1,41 @@
+# program using the comparision operator to find out wheather a given variable a is greater than b or not ?
+
+#   take a = 10 and b = 20
+
+a = 10 
+b = 20
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+    
+    
+# aab ma ekta program banauchu jasma user le fno age enter garxan rw ani elligible to vote or not bataucha
+
+age = input ("Enter your age =   ")
+
+if (int(age) > 18):
+    print("You are eligible to vote")
+else :
+    print("You are not eligible to vote")
+    
+    
+    
+    # program to find the arerage of the two numbers entered by the users
+    
+firstnum = input ("First number = ")
+secondnum = input ("Second number = \n ")
+
+average = (int(firstnum) + int(secondnum)) / 2
+
+print("The average of the two numbers is = ", average)
+
+
+# program to calculate the square of the number entered by the user
+
+num = input (" HEllo buddy enter kuch vi nmber = ")
+
+square = int (num)* int(num)
+
+print("The square of the number is = ", square)
---
+++ code with harry/chapter2/practisetest.py
@@ -0,0 +1,36 @@
+# program to add two numbers
+
+a = 4
+
+b = 10 
+
+print("The sum of the two numbers is ", + a+b)
+
+
+# program to find the remainder when a number is divided bu 7
+
+a = 44
+
+remainder = a % 7
+
+print("The remainder when", a, "is divided by 7 is ", remainder)
+
+
+# Check the type of the variable assingened using input()   function
+
+a = input("Enter a number: ")
+
+print("The type of the entered number is: ", type(a))
+
+# user le input garnu cha vane use the input() function and store that  into some variable practise again 
+
+# let's make the calculator
+
+firstnum = input ("Enter a first number: ")
+secondnum = input ("Enter a second number: ")
+
+# sum = firstnum + secondnum; yesto gare concatenate garxa 
+
+sum = int(firstnum) + int(secondnum)  # int() function is used to convert the string into integer
+
+print("The sum of the two numbers is: ", sum)
