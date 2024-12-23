ad54c4d2f099c174ff96a56548c73214122dc298
pradipbhatt <bhattsammar04@gmail.com>
2024-12-23 21:31:50 +0545

    chapter 2 operators practised

---
+++ code with harry/chapter2/practise.py
@@ -0,0 +1,28 @@
+# let's practise arthemtic operators
+
+a = 10
+b = 5
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+
+# Assingement Operators
+
+a = 5
+a += 3     # yaha nirw a = 5 + 3 hudae cha
+print("a after +=: ", a)
+
+# let's practise the comparision operators
+
+print("Is a greater than b?", a > b)
+print("Is a less than b?", a < b)
+print("Is a equal to b?", a == b)
+print("Is a not equal to b?", a!= b)
+
+
+# now let's practise the logical operators
+
+print("Is a and b both true?", a > 0 and b > 0)
+print("Is a or b either true?", a > 0 or b > 0)
+print("Is a not b true?", a > 0 and not b > 0)
+
